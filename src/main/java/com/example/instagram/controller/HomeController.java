2dbaa310fc80a291c7591407eb848161f7e11ded
qqqkyj <123rkdduswn@naver.com>
2025-12-05 10:16:20 +0900

    ãfeed 기능 구현

--- src/main/java/com/example/instagram/controller/HomeController.java
+++ src/main/java/com/example/instagram/controller/HomeController.java
@@ -1,25 +1,26 @@
 package com.example.instagram.controller;
 
-import com.example.instagram.dto.response.PostResponse;
 import com.example.instagram.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class HomeController {
 
-    private final PostService postService;
+//    private final PostService postService;
 
-    @GetMapping("/")
-    public String home(Model model) {
+//    @GetMapping("/")
+//    public String home(Model model) {
 //        List<PostResponse> postResponses = postService.getAllPosts();
-        List<PostResponse> postResponses = postService.getAllPostsWithStats();
-        model.addAttribute("posts", postResponses);
-        return "home";
-    }
+//        List<PostResponse> postResponses = postService.getAllPostsWithStats();
+//        model.addAttribute("posts", postResponses);
+//        return "home";
+//    }
+
+        @GetMapping("/")
+        public String home(){
+            return "home";
+        }
 }
